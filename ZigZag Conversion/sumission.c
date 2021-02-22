1cfcae035e3f2e6642f5f3b423c91573f14d44e1
JoowonSuh <sjw2023@naver.com>
2021-02-21 20:29:56 -0800

    6. passed all long string but still have problem with short one

---
+++ ZigZag Conversion/sumission.c
@@ -0,0 +1,39 @@
+
+
+char * convert(char * s, int numRows){
+    char *cursor = s;
+    static char ret[1000] = {0};
+    int length = 0;
+    int i = 0,j = 0;
+    int gap = (numRows*2)-2;
+    int gap2 = 0;
+    int index = 0, retIndex = 0;
+    // get length of string
+    while( *cursor++ )
+        length++;
+    if(length == 1)
+        return s;
+    while( i < numRows )
+    {
+        index = i;
+        while( index < length)
+        {
+            if(gap != 0)
+            {
+                // printf("%c\n", s[index]);
+                ret[retIndex++] = s[index];
+                index += gap;
+            }
+            if(gap2 != 0 && index < length){
+                // printf("%c\n", s[index]);
+                ret[retIndex++] = s[index];
+                index += gap2;
+            }      
+        }
+        i++;
+        gap -= 2;
+        gap2 += 2;
+    }
+
+    return ret;
+}
