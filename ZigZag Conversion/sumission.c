6e0ae8bf5f1e8c73a62fadda64c65fef2048baab
JoowonSuh <sjw2023@naver.com>
2021-02-22 13:29:08 -0800

    6. ZigZag Conversion - cus I didn't put null terminator at the end of the string, static veriable always had string from last call.

--- ZigZag Conversion/sumission.c
+++ ZigZag Conversion/sumission.c
@@ -4,14 +4,15 @@ char * convert(char * s, int numRows){
     char *cursor = s;
     static char ret[1000] = {0};
     int length = 0;
-    int i = 0,j = 0;
+    int i = 0;
     int gap = (numRows*2)-2;
     int gap2 = 0;
     int index = 0, retIndex = 0;
     // get length of string
+
     while( *cursor++ )
         length++;
-    if(length == 1)
+    if(length == 1 || numRows == 1)
         return s;
     while( i < numRows )
     {
@@ -28,12 +29,12 @@ char * convert(char * s, int numRows){
                 // printf("%c\n", s[index]);
                 ret[retIndex++] = s[index];
                 index += gap2;
-            }      
+            }
         }
         i++;
         gap -= 2;
         gap2 += 2;
     }
-
+    ret[length] = '\0';
     return ret;
 }
