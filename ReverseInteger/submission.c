828d6e2d0a259224b8fb5daf882d0c47ddb96280
JoowonSuh <sjw2023@naver.com>
2021-02-22 14:08:00 -0800

    I refered Answer, failed to guess what is the bug inside,

--- ReverseInteger/submission.c
+++ ReverseInteger/submission.c
@@ -3,8 +3,8 @@ int reverse(int x){
     int count=0;
     bool isNeg = false;
     int ten = 10;
-    int max = ( (1 << 30 )-1 )/ 10;// devided by 10 since we need to compare if x*ten is greater than max or not
-    int min = -( (1 << 30 ) ) / 10;
+    // long long max = ( (1 << 31 )-1 )/ 10;// devided by 10 since we need to compare if x*ten is greater than max or not
+    // long long min = -( (1 << 31 ) ) / 10;
     if(x == 0)
     {
         return 0;
@@ -16,13 +16,13 @@ int reverse(int x){
     }
     while( x != 0 )
     {
-        if( x > max || x < min )
-            return 0;
-        ret *= ten;
-        // printf("%d\n", ret);
-        ret += x%10;
+        int pop = x % 10;
+        if( ret > INT_MAX/10 || (ret == INT_MAX / 10 && pop > 7)) return 0;
+        if( ret < INT_MIN/10 || (ret == INT_MIN / 10 && pop < -8)) return 0;
+
+        ret = ret * ten + pop;
         x /= 10;
-        count++;
+        // count++;
     }
     return (isNeg) ? -ret : ret;
 }
