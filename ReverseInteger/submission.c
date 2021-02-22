a4b650076b61e30a48e3413fea8e5c161d5671ce
JoowonSuh <sjw2023@naver.com>
2021-02-22 13:44:59 -0800

    Added min max condition

--- ReverseInteger/submission.c
+++ ReverseInteger/submission.c
@@ -1,10 +1,10 @@
-#include <math.h>
-
 int reverse(int x){
     int ret=0;
     int count=0;
     bool isNeg = false;
     int ten = 10;
+    int max = ( (1 << 30 )-1 )/ 10;// devided by 10 since we need to compare if x*ten is greater than max or not
+    int min = -( (1 << 30 ) ) / 10;
     if(x == 0)
     {
         return 0;
@@ -16,8 +16,10 @@ int reverse(int x){
     }
     while( x != 0 )
     {
+        if( x > max || x < min )
+            return 0;
         ret *= ten;
-        printf("%d\n", ret);
+        // printf("%d\n", ret);
         ret += x%10;
         x /= 10;
         count++;
