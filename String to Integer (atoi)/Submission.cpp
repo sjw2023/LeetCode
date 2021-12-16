ab709590cb421f792a30d951201b1036e16c5986
JoowonSuh <sjw2023@users.noreply.github.com>
2021-12-15 19:12:33 -0500

    Modiffication on Loop
    Modified to print out number and '-'

--- String to Integer (atoi)/Submission.cpp
+++ String to Integer (atoi)/Submission.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     int myAtoi(string s) {
         string::iterator letter;
+        int toret = 0;
         for(letter = s.begin();  letter!= s.end(); ++letter){
-            cout << *letter << endl;
+            if( *letter == 32 ){
+                cout << "White space" << endl;
+            }else if( *letter > 47 && *letter < 58 ){
+                cout << *letter << endl;
+            }
         }
         return 0;
     }
