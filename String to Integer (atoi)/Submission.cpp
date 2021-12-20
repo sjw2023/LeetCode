32bba16e536b7a0ab30c02be89b9c1ca866bc5f7
JoowonSuh <sjw2023@users.noreply.github.com>
2021-12-20 16:02:05 -0500

    3 condition coded
    Return negative, ignore whitespace, turn into integer

--- String to Integer (atoi)/Submission.cpp
+++ String to Integer (atoi)/Submission.cpp
@@ -3,13 +3,23 @@ public:
     int myAtoi(string s) {
         string::iterator letter;
         int toret = 0;
+        bool negative = false;
         for(letter = s.begin();  letter!= s.end(); ++letter){
             if( *letter == 32 ){
-                cout << "White space" << endl;
+                //ignore
+                //cout << "White space" << endl;
             }else if( *letter > 47 && *letter < 58 ){
-                cout << *letter << endl;
+                toret *= 10;
+                toret += (*letter - 48);
+                // cout << toret << endl;
+            }else if( *letter == '-' ){
+                // cout << '-' << endl;
+                negative = true;
+            }
+            else{
+                break;
             }
         }
-        return 0;
+        return negative ? -toret : toret;
     }
 };
